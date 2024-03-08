f150e0113bb64f66685e311b5cde303c6464b1f8
PurveshMavle <162665382+PurveshMavle@users.noreply.github.com>
2024-03-08 19:39:36 +0000

    update

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,77 @@
+// const TinderUser = new Object()
+const TinderUser={}
+//1st and 2 nd line both are used for creating object but the difference is the first line is singleton object and second is non singleon object
+
+TinderUser.id ="123abc"
+TinderUser.name ="Puma"
+TinderUser.isloggedin=false
+
+// console.log(TinderUser)
+
+const regularuser ={
+    email:"some@gmail.com",
+    fullname :{
+      userfullname:{
+        firstname:"Purvesh",
+        lastname:"mavle"
+      }
+    }
+}
+console.log(regularuser.fullname.userfullname.firstname);
+
+const obj1 ={1:"a",2:"b"}
+const obj2 ={3:"c",4:"d"}
+const obj4 ={5:"e",6:"f"}
+
+// const obj3=Object.assign({},obj1,obj2,obj4)//{} act as a target and all values are copied in it and rest all act as a source
+
+const obj3={...obj1,...obj2,...obj4}//object spread operator
+// console.log(obj3);
+
+const users =[
+  {
+    id:1,
+    email:"h@gmail.com"
+  },
+  {
+    id:1,
+    email:"h@gmail.com"
+  },
+  {
+    id:1,
+    email:"h@gmail.com"
+  },
+]
+
+users[1].email
+console.log(TinderUser);
+
+
+// console.log(Object.keys(TinderUser))
+// console.log(typeof (Object.keys(TinderUser)))
+// console.log((Object.values(TinderUser)))
+// console.log((Object.entries(TinderUser)))
+
+// console.log((TinderUser.hasOwnProperty('isloggedin')))
+// console.log((TinderUser.hasOwnProperty('islogged')))
+
+const course = {
+  coursename :"js in hindi",
+  price:"999",
+  insturctor:"Purvesh"
+}
+// course.insturctor standard method
+
+const {insturctor:inst}=course//object destructuring
+console.log(inst)
+
+// {
+//   "name":"purvesh",
+//   "coursename":"jsservices",
+//   "price":"free"
+// }
+[
+  {},
+  {},
+  {}
+]
