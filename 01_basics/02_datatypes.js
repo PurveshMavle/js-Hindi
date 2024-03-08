9a7c2427e5d3681cc172b3be125dc9b8e60adf2d
PurveshMavle <162665382+PurveshMavle@users.noreply.github.com>
2024-03-08 06:05:54 +0000

    update

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"// whatever code we write of JS code  now will be treated as newer version by default it is treated directly by engine
+
+//alert(3+3) // this will show error as we are working in node not in html bind or console of browser to use it in node its syntax is different 
+
+
+console.log(3
+    +3) ;
+// code readability should be high 
+console.log("purvesh");
+
+let name="purvesh" // datatype is string
+let age = 19 // datatype is int
+let isLoggedIn = true
+let state =null  //whenever we dont have to show a value show instead of leaving it undefined we can use null
+
+//primitive datatypes:
+// number  =>2 to power 53
+//bigint=> used to display big number
+//string => ""
+//boolean => only two values "true" and "false"
+//null =>its a stand alone value hai 
+//undefined => no value assigned 
+//symbol=> whenever we use  to find uniquess of components
+
+
+//object 
+
+console.log(typeof(age)) // o/p is number
+console.log((typeof(null))); // o/p is object
+console.log(typeof(undefined)) //o/p is undefined 
